c0ac896aad78339e3887e2f9f5cbcc4a9d4eb46d
Reynard Catinding <rcatindig.yrockdev@gmail.com>
2015-04-20 18:50:25 +0800

    updated

--- system/sql/create/user_types.sql
+++ system/sql/create/user_types.sql
@@ -16,38 +16,26 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `users`
+-- Table structure for table `user_type`
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `user_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `user_id` tinyint(3) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(45) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `user_type_id` tinyint(2) DEFAULT NULL,
-  `full_name` varchar(100) NOT NULL,
-  `ministry_id` tinyint(3) DEFAULT NULL,
-  `profile_pic` varchar(100) DEFAULT NULL,
-  `created_date` datetime NOT NULL,
-  `active_flag` enum('Y','N') DEFAULT 'N',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_name_UNIQUE` (`user_name`),
-  KEY `fk_users_1_idx` (`user_type_id`),
-  KEY `fk_users_2_idx` (`ministry_id`),
-  CONSTRAINT `fk_users_1` FOREIGN KEY (`user_type_id`) REFERENCES `user_type` (`user_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_users_2` FOREIGN KEY (`ministry_id`) REFERENCES `ministry` (`ministry_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `user_type` (
+  `user_type_id` tinyint(2) NOT NULL AUTO_INCREMENT,
+  `user_type` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_type_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `user_type`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+LOCK TABLES `user_type` WRITE;
+/*!40000 ALTER TABLE `user_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_type` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -59,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-20 18:44:05
+-- Dump completed on 2015-04-20 18:12:09
