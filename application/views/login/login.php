6a4e83d6f9e4da34d33963ac34722786c44e3739
John Gabriel Raymudo <john.raymundo@directwithhotels.com>
2015-04-28 17:45:39 +0800

    Client login validation. re #9

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,3 +1,4 @@
+
 <div class="container">
 	<div class="page-header" align="center">
 	    <h1>test</h1>
@@ -26,13 +27,16 @@
                                 <i class="fa fa-arrows-v"></i> Signup Form <i class="fa fa-arrows-v"></i>
                             </h3>
 		                    <div class="panel-body">
-		                    <?=Form::open('login/signup', array('class'=>'search_form'));?>
-		                    	<?=Form::input('full_name', '', ['placeholder'=>'Full Name', 'name'=>'username']);?>
-				            	<?=Form::input('username', '', ['placeholder'=>'Username', 'name'=>'username']);?>
+		                    <?=Form::open('login/signup', array('class'=>'search_form', 'id'=>'signup_form'));?>
+		                    	<div class="full-name-error">
+		                    		<?=Form::input('full_name', '', ['placeholder'=>'Full Name', 'name'=>'full_name', 'class'=>'redborder']);?>
+		                    	</div>
+		                    	<div class="user-name-error">
+				            		<?=Form::input('username', '', ['placeholder'=>'Username', 'name'=>'username', 'class'=>'redborder']);?>
+					          	</div>
 					          	<?=Form::password('password', '', ['placeholder'=>'Password', 'name'=>'password']);?>
 					            <?=Form::password('password_confirm', '', ['placeholder'=>'Password', 'name'=>'password_confirm']);?>
-					            <?=Form::button('sign_u[', 'Sign Up', array('type' => 'submit', 'class'=>'btn btn-default wide fa medhidden redborder'));?>
-
+					            <?=Form::button('sign_up', 'Sign Up', array('type' => 'submit', 'class'=>'btn btn-default wide fa medhidden redborder'));?>
 					        <?=Form::close();?>
 		                    </div>
 		                </div>
@@ -43,3 +47,36 @@
 
     </div><!--end of row-->
 </div><!--end of container-->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><!--for test only-->
+<script type="text/javascript">
+	$(document).ready(function() {
+	$('#signup_form').formValidation({
+		message: 'This value is not valid',
+		icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+	        full_name: {
+                row: '.full-name-error',
+                validators: {
+                    notEmpty: {
+                        message: 'The full name is required'
+                    }
+                }
+        	},
+
+        	username: {
+                row: '.user-name-error',
+                validators: {
+                    notEmpty: {
+                        message: 'The user name is required'
+                    }
+                }
+        	}
+        }
+	});
+	});
+</script>
