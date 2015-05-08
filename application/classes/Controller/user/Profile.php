2d90ea139810ccf329ee251dfc2d79ec3c2b89aa
Jem Raymundo <jeremuel.raymundo@nrnssoftware.com.au>
2015-05-08 15:58:21 +0800

    update develop

--- application/classes/Controller/user/Profile.php
+++ application/classes/Controller/user/Profile.php
@@ -4,7 +4,7 @@ class Controller_User_Profile extends Controller_Base {
 
     public function before()
     {
-    	$this->_is_logged_in();
+        $this->_is_logged_in();
         
         parent::before();
 
@@ -13,20 +13,20 @@ class Controller_User_Profile extends Controller_Base {
     public function action_index()
     {
         $auth_ins = Auth::instance()->get_user();
-    	$username = Auth::instance()->get_user()->username;
-    	$current_ministry =Auth::instance()->get_user()->ministry_id;
-		$ministries = ORM::factory('Ministry')->find_all();
+        $username = Auth::instance()->get_user()->username;
+        $current_ministry =Auth::instance()->get_user()->ministry_id;
+        $ministries = ORM::factory('Ministry')->find_all();
 
-    	if (HTTP_Request::POST == $this->request->method()) 
+        if (HTTP_Request::POST == $this->request->method()) 
         {
             $post = $this->request->post();
         }
         
         $this->template->body = View::factory('user/profile')
-        									->bind('username', $username)
-        									->bind('auth_ins', $auth_ins)
-        									->bind('ministries', $ministries)
-        									->bind('current_ministry',$current_ministry);
+                                            ->bind('username', $username)
+                                            ->bind('auth_ins', $auth_ins)
+                                            ->bind('ministries', $ministries)
+                                            ->bind('current_ministry',$current_ministry);
     }
         
 } // End of class
