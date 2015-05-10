4756673a3b30e4435ea641131f991417bec3083a
joshua fradejas <jfradejas.yrockdev@gmail.com>
2015-05-10 12:49:43 +0800

    asdf

---
+++ application/classes/Controller/Admin/Ministry.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Admin_Ministry extends Controller_Base {
+
+    public function before()
+    {
+        parent::before();
+    }
+    
+    public function action_index()
+    {
+        echo "this is the landing page of t"; die;
+    }
+        
+} // End of class
