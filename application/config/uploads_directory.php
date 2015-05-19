9e2e5ac64956cd9da7fb8c3c5f888b364f10059f
Jem Raymundo <jeremuel.raymundo@nrnssoftware.com.au>
2015-05-19 12:37:26 +0800

    add config file for absolute and relative path

---
+++ application/config/uploads_directory.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+	return array
+	(
+		'root' => array(
+			'avatar' => array(
+				'absolute' => DOCROOT.'uploads'.DIRECTORY_SEPARATOR,
+				'relative' => URL::base().'uploads'.'/'.'avatar'.'/'
+			)
+		)
+	);
