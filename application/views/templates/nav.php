253729cfa44914fa6f56016ede727ce6cbd75418
unknown <jraymundo.yrockdev@gmail.com>
2015-07-18 12:24:50 +0800

    Trim down files

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/dashboard">Rogubukku - <small><strong>G</strong>olden <strong>F</strong>aith <strong>C</strong>enter <strong>C</strong>hristian <strong>M</strong>inistries </small></a>
+        <a class="navbar-brand" href="/dashboard">Rogubukku <small>(beta 1.0)</small> - <small><strong>G</strong>olden <strong>F</strong>aith <strong>C</strong>enter <strong>C</strong>hristian <strong>M</strong>inistries </small></a>
     </div>
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
